cfc9bbcbc9e65e8c77b261b648a303aceba7744d
Alwin Tareen <altareen@gmail.com>
2019-09-06 08:43:55 +0800

    deploy website

---
+++ 01PythonSyntax/arithmetic.py
@@ -0,0 +1,54 @@
+total = 23 + 14
+print(total)
+
+difference = 29 - 83
+print(difference)
+
+product = 92 * 65
+print(product)
+
+# decimal divide gives a calculator-style result
+calcdivide = 982 / 37
+print(calcdivide)
+
+# integer divide is the same as the
+# mathematical floor operation
+intdivide = 927 // 83
+print(intdivide)
+
+power = 17**5
+print(power)
+
+# perform a square root
+sqrt = 25**(0.5)
+print(sqrt)
+
+# perform a cube root
+cube = 64**(1/3)
+print(cube)
+
+# perform a nth root
+root = 9328**(1/17)
+print(root)
+
+# modulus gives you the remainder
+remain = 7%3
+print(remain)
+
+leftover = 25%11
+print(leftover)
+
+# determine if a number is even
+score = 10
+result = score%2
+print(result == 0)
+
+# determine if a number is odd
+tally = 17
+outcome = tally%2
+print(outcome == 1)
+
+
+
+
+
---
+++ 01PythonSyntax/mathematical.py
@@ -0,0 +1,55 @@
+total = 23 + 89
+print(total)
+
+score = 82 - 76
+print(score)
+
+product = 91 * 65
+print(product)
+
+# decimal divide
+result = 92 / 37
+print(result)
+
+# integer divide is the same as the
+# mathematical floor operation
+outcome = 87 // 19
+print(outcome)
+
+margin = 8**5
+print(margin)
+
+profit = 2**14
+print(profit)
+
+root = 25**0.5
+print(root)
+
+cube = 64**(1/3)
+print(cube)
+
+rootout = 8237**(1/9)
+print(rootout)
+
+# modulus operator gives the remainder
+print(7%3)
+print(25%11)
+
+# check for an even number
+retail = 10
+answer = retail%2
+print(answer == 0)
+
+# check for an odd number
+sale = 17
+solution = sale%2
+print(solution == 1)
+
+reagent = 92.8796
+completion = round(reagent, 1)
+print(completion)
+
+
+
+
+
