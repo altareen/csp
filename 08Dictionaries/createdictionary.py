eaee077804beecec75062026b421e48218fb182b
Alwin Tareen <altareen@gmail.com>
2019-11-11 16:38:03 +0800

    deploy website

---
+++ 08Dictionaries/createdictionary.py
@@ -0,0 +1,11 @@
+# creating an empty dictionary
+sports = {}
+print(sports)
+
+# creating a dictionary with key-value pairs
+fruit = {"pear":2, "banana":3, "orange":5}
+print(fruit)
+
+# looking up a value from a dictionary
+value = fruit["orange"]
+print(value)
---
+++ 08Dictionaries/makedictionary.py
@@ -0,0 +1,21 @@
+# creating an empty dictionary
+sports = {}
+print(sports)
+
+# creating a dictionary with some initial key-value pairs
+fruit = {"pear":2, "banana":3, "orange":5}
+value = fruit["orange"]
+print(value)
+
+phonebook = {"alice":9283, "bob":8237, "carl":3346}
+number = phonebook["bob"]
+print(number)
+
+# adding a new key-value pair to the fruit dictionary
+fruit["mango"] = 12
+print(fruit)
+
+# adding a new key-value pair to the phonebook dictionary
+phonebook["dan"] = 1276
+print(phonebook)
+
