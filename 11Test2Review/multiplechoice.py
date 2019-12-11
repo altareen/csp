fda491a34df0b01fca2fa0507a24c45d695b8374
Alwin Tareen <altareen@gmail.com>
2019-12-11 16:09:05 +0800

    deploy website

--- 11Test2Review/multiplechoice.py
+++ 11Test2Review/multiplechoice.py
@@ -125,3 +125,109 @@ print(food)
 # 8
 treasure = {"gold":50, "silver":100}
 print("gold" in treasure)
+
+# 9.
+inventory = {
+    "pocket":"lint",
+    "canteen":"water",
+    "pouch":"flint",
+    "backpack":["shovel", "bedroll", "rope"]
+}
+print(inventory["backpack"])
+
+# 10.
+fortune = {"gold":500}
+fortune["gold"] += 50
+print(fortune)
+
+# 11.
+inventory = {
+    "gold":500,
+    "backpack":["xylophone", "dagger", "bedroll"]
+}
+inventory["backpack"].sort()
+print(inventory["backpack"])
+print(inventory)
+
+# 12.
+grocery = {"kiwi":5, "grape":12}
+del grocery["kiwi"]
+print(grocery)
+
+# 13.
+salad = {"caesar":1, "garden":2}
+salad["vegetable"] = 3
+print(salad)
+
+# 14.
+singer = {"justin":"bieber", "taylor":"swift", "ed":"sheeran"}
+print(singer.get("swift", "guitar"))
+
+# 15.
+sports = {"tennis":43, "football":78, "badminton":52}
+result = list(sports.keys())
+print(result)
+
+# Part 3: Python Programming
+
+# Question 1.
+def inventory():
+    '''
+    returns a float, the total value of all the products.
+    '''
+    prices = {"banana":4, "apple":2, "orange":1.5, "pear":3}
+    stock = {"banana":6, "apple":7, "orange":31, "pear":15}
+    # YOUR CODE HERE
+    total = 0
+    for item in prices:
+        product = prices[item] * stock[item]
+        total += product
+    return total
+
+result = inventory()
+print(result)
+
+# Question 2.
+def transcription(dna):
+    mapping = {"A": "U", "C": "G", "G": "C", "T": "A"}
+    rna = ""
+    for letter in dna:
+        codon = mapping[letter]
+        rna += codon
+    return rna
+
+result = transcription("AGGCTACGT")
+print(result)
+
+# Question 3.
+def convertmandarin(eng):
+    nums = {"0": "ling", "1": "yi", "2": "er", "3": "san", "4": "si", "5": "wu", "6": "liu", "7": "qi", "8": "ba", "9": "jiu", "10": "shi"}
+    result = ""
+    value = int(eng)
+    if 0 <= value <= 9:
+        result = nums[eng]
+    elif 10 <= value <= 19:
+        digit = eng[1]
+        result = "shi" + " " + nums[digit]
+    elif 20 <= value <= 99:
+        first = eng[0]
+        second = eng[1]
+        if second == "0":
+            result = nums[first] + " shi"
+        else:
+            result = nums[first] + " shi " + nums[second]
+    return result
+
+result = convertmandarin("3")
+print(result)
+result = convertmandarin("16")
+print(result)
+result = convertmandarin("36")
+print(result)
+result = convertmandarin("20")
+print(result)
+
+
+
+
+
--- 11Test2Review/part1multiplechoice.py
+++ 11Test2Review/part1multiplechoice.py
@@ -144,3 +144,85 @@ fortune = {"gold":500}
 fortune["gold"] += 50
 print(fortune)
 
+# 11.
+inventory = {
+"gold":500,
+"backpack":["xylophone", "dagger", "bedroll"]
+}
+inventory["backpack"].sort()
+print(inventory["backpack"])
+
+# 12.
+grocery = {"kiwi":5, "grape":12}
+del grocery["kiwi"]
+print(grocery)
+
+# 13.
+salad = {"caesar":1, "garden":2}
+salad["vegetable"] = 3
+print(salad)
+
+# 14.
+singer = {"justin":"bieber", "taylor":"swift", "ed":"sheeran"}
+print(singer.get("swift", "guitar"))
+
+# 15.
+sports = {"tennis":43, "football":78, "badminton":52}
+result = list(sports.keys())
+print(result)
+
+# Part 3: Python Programming
+
+# Question 1.
+def inventory():
+    prices = {"banana":4, "apple":2, "orange":1.5, "pear":3}
+    stock = {"banana":6, "apple":7, "orange":31, "pear":15}
+    total = 0.0
+    for item in prices:
+        product = prices[item] * stock[item]
+        total += product
+    return total
+
+result = inventory()
+print(result)
+
+# Question 2.
+def transcription(dna):
+    mapping = {"A": "U", "C": "G", "G": "C", "T": "A"}
+    rna = ""
+    for letter in dna:
+        codon = mapping[letter]
+        rna += codon
+    return rna
+
+result = transcription("AGGCTACGT")
+print(result)
+
+# Question 3.
+def convertmandarin(eng):
+    nums = {"0": "ling", "1": "yi", "2": "er", "3": "san", "4": "si", "5": "wu", "6": "liu", "7": "qi", "8": "ba", "9": "jiu", "10": "shi"}
+    result = ""
+    value = int(eng)
+    if 0 <= value <= 10:
+        result = nums[eng]
+    elif 11 <= value <= 19:
+        digit = eng[1]
+        result = "shi " + nums[digit]
+    elif 20 <= value <= 99:
+        first = eng[0]
+        second = eng[1]
+        if second == "0":
+            result = nums[first] + " shi"
+        else:
+            result = nums[first] + " shi " + nums[second]
+    return result
+
+result = convertmandarin("3")
+print(result)
+result = convertmandarin("16")
+print(result)
+result = convertmandarin("36")
+print(result)
+result = convertmandarin("20")
+print(result)
+
